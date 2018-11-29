b0b37d8db3e5df9d1c34285f422f653ae6fdbe2f
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-29 15:40:15 -0600

    made the correct url for the earnings

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 urlpatterns = [
     path('add/', views.Add.as_view(), name='add'),
     path('double/', views.Double.as_view(), name="double"),
-    path('triple/', views.Triple.as_view(), name='triple')
+    path('triple/', views.Triple.as_view(), name='triple'),
+    path('earnings/', views.Earnings.as_view(), name='earnings')
 ]
