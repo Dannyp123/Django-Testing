2aac0ffb365f6f10d38ee73436e237a84ae59fce
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-29 13:42:10 -0600

    Made the url for the triple class and get method

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 urlpatterns = [
     path('add/', views.Add.as_view(), name='add'),
     path('double/', views.Double.as_view(), name="double")
+    path('triple/', views.Triple.as_view(), name='triple')
 ]
