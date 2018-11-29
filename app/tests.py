abd5925de2fc8e4b13487ac6909dd9ec6a702f20
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-29 11:25:42 -0600

    first inital commit

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,117 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestAddCanHandleSimpleAddition(SimpleTestCase):
+    '''
+    0+0 = 0
+    2+2=4
+    2.3 + 1.2 = 3.5
+    2+-1 = 1
+    -2+-3 = -5
+
+    '''
+
+    def test_two_plus_two(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": '2',
+                "num2": '2'
+            },
+        )
+
+        self.assertEqual(response.context['answer'], 4)
+
+    def test_two_plus_negative_one(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": '2',
+                "num2": '-1'
+            },
+        )
+        self.assertEqual(response.context["answer"], 1)
+
+    def test_zero_plus_zero(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": '0',
+                "num2": '0'
+            },
+        )
+
+        self.assertEqual(response.context["answer"], 0)
+
+    def test_float_plus_float(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": '2.3',
+                "num2": '1.2'
+            },
+        )
+
+        self.assertEqual(response.context["answer"], 3.5)
+
+    def test_neg_plus_neg(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": '-2',
+                "num2": '-3'
+            },
+        )
+
+        self.assertEqual(response.context["answer"], -5)
+
+
+class TestAddWithoutNumbers(SimpleTestCase):
+    ''' If add is not given two numbers it should present the user with the add.html template and not try to compute and answer'''
+
+    def test_given_non_numeric_input(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": 'a',
+                "num2": 'b'
+            },
+        )
+
+        self.assertTemplateUsed(response, 'app/add.html')
+        self.assertNotIn("answer", response.context)
+
+    def test_given_one_empty_inputs(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": " ",
+                "num2": "1"
+            },
+        )
+
+        self.assertTemplateUsed(response, 'app/add.html')
+        self.assertNotIn("answer", response.context)
+
+    def test_given_two_empty_inputs(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": " ",
+                "num2": " "
+            },
+        )
+        self.assertTemplateUsed(response, 'app/add.html')
+        self.assertNotIn("answer", response.context)
+
+    def test_given_two_list(self):
+        response = self.client.get(
+            path=reverse('add'),
+            data={
+                "num1": "[1,2,3,4]",
+                "num2": "[5,6,7,8]"
+            },
+        )
+        self.assertTemplateUsed(response, 'app/add.html')
+        self.assertNotIn('answer', response.context)
--- app/views.py
+++ app/views.py
@@ -4,13 +4,28 @@ from django.views import View
 
 class Add(View):
     def get(self, request):
-        num1 = request.GET.get('num1')
-        num2 = request.GET.get('num2')
-
-        if num1 is not None and num2 is not None:
-            num1 = float(num1)
-            num2 = float(num2)
-            answer = num1 + num2
-            return render(request, 'app/add.html', {'answer': answer})
-        else:
+        try:
+            num1 = float(request.GET.get('num1'))
+            num2 = float(request.GET.get('num2'))
+        except ValueError:
             return render(request, 'app/add.html')
+        else:
+            return render(request, 'app/add.html', {'answer': num1 + num2})
+
+
+class Double(View):
+    def get(self, request):
+        try:
+            number1 = float(request.GET.get('number1'))
+        except ValueError:
+            return render(request, 'app/double.html', {'solution': solution})
+        else:
+            solution = number1 * 2
+            return render(request, 'app/double.html')
+
+
+class Triple(View):
+    def get(self, request):
+        num1 = request.GET.get('num1')
+        num2 = request.GET.get("num2")
+        num3 = request.GET.get("num3")
