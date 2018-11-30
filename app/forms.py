93feda5cd60dc06007e62ef170689ddaec36d249
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-30 09:32:06 -0600

    started to made django forms for the test I am making

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+
+class AddForm(forms.Form):
+    num1 = forms.FloatField()
+    num2 = forms.FloatField()
+
+
+class DoubleForm(forms.Form):
+    num1 = forms.FloatField()
+
+
+class TripleForm(forms.Form):
+    num1 = forms.FloatField()
+
+
+class EarningsForm(forms.Form):
+    seat_a = forms.IntegerField()
+    seat_b = forms.IntegerField()
+    seat_c = forms.IntegerField()
