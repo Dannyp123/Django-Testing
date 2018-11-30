6d13f2e68b85c258e420918adfd0e0ef27353fc6
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-30 14:55:39 -0600

    made a  validation with a min_value = 0 to keep from below zero inputs

--- app/forms.py
+++ app/forms.py
@@ -26,5 +26,5 @@ class BothForm(forms.Form):
 
 
 class WalkOrDrive(forms.Form):
-    distance_input = forms.FloatField()
+    distance_input = forms.FloatField(min_value=0)
     is_nice_weather_input = forms.BooleanField(required=False)
