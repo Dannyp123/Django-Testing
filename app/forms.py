3ea6ae30af854e75b8fde8a519ebc7123f2f8a2b
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-30 15:34:09 -0600

    had to add that input to the form

--- app/forms.py
+++ app/forms.py
@@ -28,3 +28,8 @@ class BothForm(forms.Form):
 class WalkOrDrive(forms.Form):
     distance_input = forms.FloatField(min_value=0)
     is_nice_weather_input = forms.BooleanField(required=False)
+
+
+class HowPopulate(forms.Form):
+    population_input = forms.FloatField(min_value=0)
+    land_area_input = forms.FloatField(min_value=0)
