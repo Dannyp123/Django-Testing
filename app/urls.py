74fd37f6e23610f14428ba172e96cd28ae6d5607
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-30 15:59:34 -0600

    url to the gold stars view

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('earnings/', views.Earnings.as_view(), name='earnings'),
     path('both/', views.Both.as_view(), name='both'),
     path('walk_or_drive/', views.WalkOrDrive.as_view(), name='walk_or_drive'),
-    path('how_populated/', views.HowPopulated.as_view(), name='how_populated')
+    path('how_populated/', views.HowPopulated.as_view(), name='how_populated'),
+    path('gold_stars/', views.GoldStars.as_view(), name='gold_stars')
 ]
