16b4880b182e44e9a8234d476671d47a7726fcfe
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-30 13:14:04 -0600

    made the url for the walk or drive problem

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('double/', views.Double.as_view(), name="double"),
     path('triple/', views.Triple.as_view(), name='triple'),
     path('earnings/', views.Earnings.as_view(), name='earnings'),
-    path('both/', views.Both.as_view(), name='both')
+    path('both/', views.Both.as_view(), name='both'),
+    path('walk-or-drive/', views.WalkOrDrive.as_view(), name='walk_or_drive')
 ]
